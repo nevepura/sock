7c12218df3d07bb216414c8201d9314bb198e90e
nevepura <federico.beach@gmail.com>
2026-03-25 22:08:33 +0100

    Add timestamp

--- src/main/java/com/example/sock/GreetingController.java
+++ src/main/java/com/example/sock/GreetingController.java
@@ -5,6 +5,10 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 @Controller
 public class GreetingController {
 
@@ -14,8 +18,13 @@ public class GreetingController {
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(1000);
 
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String time = LocalTime.now().format(fmt);
+
         Greeting greeting = new Greeting();
-        greeting.setContent("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        greeting.setContent(time + " - " + HtmlUtils.htmlEscape(message.getName()));
+
         return greeting;
     }
 
